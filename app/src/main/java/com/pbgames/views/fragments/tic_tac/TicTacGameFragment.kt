a74d9c76e9df3e75a2c86545b83b3d734ddd6caa
aadityakumarverma <raushankv217@gmail.com>
2025-09-16 18:12:00 +0530

    feat(tic_tac): added listeners to boxes with cross and circle icon

--- app/src/main/java/com/pbgames/views/fragments/tic_tac/TicTacGameFragment.kt
+++ app/src/main/java/com/pbgames/views/fragments/tic_tac/TicTacGameFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import com.pbgames.R
@@ -15,6 +16,12 @@ class TicTacGameFragment : Fragment() {
     lateinit var binding: FragmentTicTacGameBinding
     lateinit var sharedPreferencesHelper: SharedPreferencesHelper
     lateinit var navController: NavController
+
+    lateinit var listBoxes : ArrayList<ImageView>
+
+    var toggle = "cross"
+    val crossContains = arrayListOf<String>()
+    val circleContains = arrayListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,5 +37,28 @@ class TicTacGameFragment : Fragment() {
     ): View? {
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.apply {
+            listBoxes = arrayListOf(ivBox00,ivBox01,ivBox02,ivBox11,ivBox12,ivBox13,ivBox21,ivBox22,ivBox23)
+
+            setListeners(listBoxes)
+        }
+    }
+
+    private fun setListeners(boxes: ArrayList<ImageView>) {
+
+        boxes.forEach { box ->
+            box.setOnClickListener {
+                box.setImageResource(if (toggle == "cross") R.drawable.ic_cross else R.drawable.ic_circle)
+                box.isEnabled = false
+                toggle = if (toggle == "cross") "circle" else "cross"
+            }
+
+        }
     }
 }
